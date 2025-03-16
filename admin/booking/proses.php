71fc6f85cdb124473ffe04e99d0f05d9294f82eb
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-16 15:01:55 +0800

    fix: enhance booking process

--- admin/booking/proses.php
+++ admin/booking/proses.php
@@ -1,26 +1,24 @@
 <?php
-/*
-  | Source Code Aplikasi Rental Mobil PHP & MySQL
-  | 
-  | @package   : rental_mobil
-  | @file	   : proses.php 
-  | @author    : alkawsar / Andi Muh Raihan Alkawsar
-  | @copyright : Copyright (c) 2017-2021 Codekop.com (https://www.codekop.com)
-  | @blog      : https://www.codekop.com/products/source-code-aplikasi-rental-mobil-php-mysql-7.html 
-  | 
-  | 
-  | 
-  | 
- */
-  require '../../koneksi/koneksi.php';
 
-  if($_GET['id'] == 'konfirmasi')
-  {
-    $data2[] = $_POST['status'];
-    $data2[] = $_POST['id_booking'];
-    $sql2 = "UPDATE `booking` SET `konfirmasi_pembayaran`= ? WHERE id_booking= ?";
-    $row2 = $koneksi->prepare($sql2);
-    $row2->execute($data2);
+require_once '../../koneksi/koneksi.php';
 
-    echo '<script>alert("Kirim Sukses , Pembayaran berhasil");history.go(-1);</script>'; 
-  }
+if ($_GET['id'] == 'konfirmasi') {
+  $data2[] = $_POST['status'];
+  $data2[] = $_POST['id_booking'];
+  $sql2 = "UPDATE `booking` SET `konfirmasi_pembayaran`= ? WHERE id_booking= ?";
+  $row2 = $koneksi->prepare($sql2);
+  $row2->execute($data2);
+
+  echo "
+  <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+  <script>
+  Swal.fire({
+    icon: 'success',
+    title: 'Berhasil!',
+    text: 'Kirim Sukses, Pembayaran berhasil!',
+    showConfirmButton: true
+    }).then(() => {
+      history.go(-1);
+      });
+      </script>";
+    }
