72ba38c80cefe284b348dbca30e529d6d89d0e48
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-16 15:01:59 +0800

    fix: update database connection

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,31 +1,25 @@
 <?php
-/*
-  | Source Code Aplikasi Rental Mobil PHP & MySQL
-  | 
-  | @package   : rental_mobil
-  | @file	   : koneksi.php 
-  | @author    : alkawsar / Andi Muh Raihan Alkawsar
-  | @copyright : Copyright (c) 2017-2021 Codekop.com (https://www.codekop.com)
-  | @blog      : https://www.codekop.com/products/source-code-aplikasi-rental-mobil-php-mysql-7.html 
-  | 
-  | 
-  | 
-  | 
- */
-  $user = 'root';
-  $pass = '';
 
-  $koneksi = new PDO("mysql:host=localhost;dbname=school_pweb_rental_mobil", $user, $pass);
+$user = 'root';
+$pass = '';
 
-  global $url;
-  // $url = "http://localhost/rental_mobil/";
-  $url = "http://localhost:8005/";
+$koneksi = new PDO("mysql:host=localhost;dbname=school_pweb_rental_mobil", $user, $pass);
 
-  $sql_web = "SELECT * FROM infoweb WHERE id = 1";
-  $row_web = $koneksi->prepare($sql_web);
-  $row_web->execute();
-  global $info_web;
-  $info_web = $row_web->fetch(PDO::FETCH_OBJ);
+global $url;
+$url = "http://localhost/rental_mobil/";
 
-  error_reporting(0);
+$sql_web = "SELECT * FROM infoweb WHERE id = 1";
+
+$row_web = $koneksi->prepare($sql_web);
+
+$row_web->execute();
+
+global $info_web;
+
+$info_web = $row_web->fetch(PDO::FETCH_OBJ);
+
+
+
+error_reporting(0);
+
 ?>
